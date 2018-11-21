7dcbbbe86a0f06ed24b477ae04d1507c1e4293f5
dgkzoo <dgookz@gmail.com>
2018-11-22 02:09:47 +0900

    chore: HardWare

--- HardWare.sv
+++ HardWare.sv
@@ -1,5 +1,18 @@
-// トップレベル
-module HardWare(input clk);
-    always @(posedge clk) begin
-    end
+// Top Level
+module HardWare(
+	input clk,
+	input [2:0] btn,
+	input [9:0] sw,
+	output [9:0] led,
+	output [7:0] hled0,
+	output [7:0] hled1,
+	output [7:0] hled2,
+	output [7:0] hled3
+);
+
+	Computer computer(
+		.clk(clk),
+		.reset(0)
+	);
+	
 endmodule
