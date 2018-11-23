de792514381137abf8333a5e3a201d573c2656ee
dgkzoo <dgookz@gmail.com>
2018-11-24 03:42:04 +0900

    fix: カウンタのTB

--- test/Computer_Counter_tb.sv
+++ test/Computer_Counter_tb.sv
@@ -28,33 +28,12 @@ module Computer_Counter_tb();
 
 	always @(posedge clk) begin
 		if (debug_writeM == 1) begin
-			if(debug_outM != 1) $display("#1 debug_outM ng");
+			$display("%d", debug_outM);
 		end
-		// if (debug_pc == 15'd50001) begin
-		// 	if(debug_outM != 1) $display("#1 debug_outM ng");
-		// end
-		// if (debug_pc == 15'd50002) begin
-		// 	if(debug_outM != 1) $display("#1 debug_outM ng");
-		// end
-		// if (debug_pc == 15'd50003) begin
-		// 	if(debug_outM != 1) $display("#1 debug_outM ng");
-		// end
-		// if (debug_pc == 15'd100000) begin
-		// 	if(debug_outM != 2) $display("#2 debug_outM ng");
-		// end
 	end
 
 	initial begin
 		$display("start test");
-
-		// テストintruction Computer.svのInstructionRomに記載する内容
-		/*
-		0: InstructionRom = 16'b0_000_0000_0000_0000;	// @0
-		1: InstructionRom = 16'b111_0_110000_010_000;	// D=A :to D
-		50000: InstructionRom = 16'b111_0_011111_111_111;	// D+1 :to AMD
-		50001: InstructionRom = 16'b0_000_0000_0000_0001;	// @1
-		50002: InstructionRom = 16'b111_0_101010_000_111;	// 0;JMP 
-		*/
 
 		#99999999
 		$display("success!");
