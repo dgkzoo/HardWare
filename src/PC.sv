f4c13e82c9c734b93f96934f97f8bd24a522e131
dgkzoo <dgookz@gmail.com>
2018-11-25 00:44:09 +0900

    refactor: Add16対応

--- src/PC.sv
+++ src/PC.sv
@@ -2,13 +2,16 @@
 `default_nettype none
 module PC(
 	input wire clk,				// clk
-	input wire[15:0] in,			// in値
+	input wire[15:0] in,		// in値
 	input wire inc,				// incliment flg
-	input wire load,				// write enable flg
+	input wire load,			// write enable flg
 	input wire reset,			// reset flg
 	output wire[15:0] out);		// out
 
 	reg[15:0] counter = 0;
+	wire[15:0] added;
+
+	Add16 add16(.a(counter), .b(16'd1), .out(added));
 
 	always @(posedge clk) begin
 		if(reset == 1'b1) begin
@@ -18,7 +21,7 @@ module PC(
 			counter <= in;
 
 		end else if(inc == 1'b1) begin
-			counter <= counter + 16'd1;
+		 	counter <= added;
 		end
 	end
 
--- test/Add16_tb.sv
+++ test/Add16_tb.sv
@@ -6,6 +6,8 @@ module Add16_tb();
 	reg[15:0] b;
 	wire[15:0] sum;
 
+	parameter RATE = 1;
+
 	always #1 clk = !clk;
 
 	Add16 add16(
@@ -18,22 +20,46 @@ module Add16_tb();
 		$display("start test");
 		clk <= 1;
 
-		#1
+		#(RATE)
 		a <= 0;
 		b <= 0;
-		#2
+		#(RATE)
 		if (sum != 0) $display("0 0 sum ng");
 
-		#3
+		#(RATE)
 		a <= 1;
 		b <= 0;
-		#4
+		#(RATE)
 		if (sum != 1) $display("0 0 sum ng");
 
-		#5
+		#(RATE)
+		a <= 16'd1;
+		b <= 16'd1;
+		#(RATE)
+		if (sum != 16'd2) $display("1 1 sum ng");
+
+		#(RATE)
+		a <= -16'd1;
+		b <= 16'd1;
+		#(RATE)
+		if (sum != 16'd0) $display("-1 1 sum ng");
+
+		#(RATE)
+		a <= 16'd123;
+		b <= 16'd456;
+		#(RATE)
+		if (sum != 16'd579) $display("123 456 sum ng");
+
+		#(RATE)
+		a <= 16'd123;
+		b <= -16'd456;
+		#(RATE)
+		if (sum != -16'd333) $display("123 -456 sum ng");
+
+		#(RATE)
 		a <= 16'b1111111111111111;
 		b <= 16'b1111111111111111;
-		#6
+		#(RATE)
 		if (sum != 16'b1111111111111110) $display("16'b1111111111111111 16'b1111111111111111 sum ng");
 
 		#9999999
