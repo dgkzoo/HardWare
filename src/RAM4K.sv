c01a8135c3bafacd909d90b12083989528f9ef76
dgkzoo <dgookz@gmail.com>
2018-11-25 01:34:30 +0900

    chore

--- src/RAM4K.sv
+++ src/RAM4K.sv
@@ -2,9 +2,9 @@
 `default_nettype none
 module RAM4K(
 	input wire clk,				// clk
-	input wire[15:0] in,			// in値
+	input wire[15:0] in,		// in値
 	input wire[15:0] address,	// アドレス
-	input wire load,				// write enable
+	input wire load,			// write enable
 	output wire[15:0] out);		// out
 
 	reg[15:0] RAM[4096-1:0];
