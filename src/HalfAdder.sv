163b0bb5b4a08ac3e4a2535e7f3a00862607fe33
dgkzoo <dgookz@gmail.com>
2018-11-25 00:16:09 +0900

    refactor: And対応

--- src/HalfAdder.sv
+++ src/HalfAdder.sv
@@ -3,9 +3,9 @@
 module HalfAdder(
 	input wire a,		// 値１
 	input wire b,		// 値２
-	output wire sum,		// 結果
+	output wire sum,	// 結果
 	output wire carry);	// キャリーフラグ
  
 	assign sum = a ^ b;
-	assign carry = a & b;
+	_And and1(.a(a), .b(b), .out(carry));
 endmodule
