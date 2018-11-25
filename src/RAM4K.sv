ff36215e369104a8a6ef3d923f15f04665b7c496
dgkzoo <dgookz@gmail.com>
2018-11-26 02:10:41 +0900

    fix: RAM4Kはverilogのを使う(+_+) 論理合成終わらないため。。

--- src/RAM4K.sv
+++ src/RAM4K.sv
@@ -7,30 +7,47 @@ module RAM4K(
 	input wire load,			// write enable
 	output wire[15:0] out);		// out
 
-	// アドレスをセレクタとして、load値をload0～7に振り分ける
-	wire load0,load1,load2,load3,load4,load5,load6,load7;
-	DMux8Way dmux8(
-		.in(load),
-		.sel(address[11:9]),
-		.a(load0), .b(load1), .c(load2), .d(load3),
-		.e(load4), .f(load5), .g(load6), .h(load7));
+	reg[15:0] RAM[4096-1:0];
 
-	// load0～7でRegister0～7をロード、またはリードする
-	wire[15:0] out0,out1,out2,out3,out4,out5,out6,out7;
-	RAM512 ram0(.clk(clk), .in(in), .load(load0), .address(address[8:0]), .out(out0));
-	RAM512 ram1(.clk(clk), .in(in), .load(load1), .address(address[8:0]), .out(out1));
-	RAM512 ram2(.clk(clk), .in(in), .load(load2), .address(address[8:0]), .out(out2));
-	RAM512 ram3(.clk(clk), .in(in), .load(load3), .address(address[8:0]), .out(out3));
-	RAM512 ram4(.clk(clk), .in(in), .load(load4), .address(address[8:0]), .out(out4));
-	RAM512 ram5(.clk(clk), .in(in), .load(load5), .address(address[8:0]), .out(out5));
-	RAM512 ram6(.clk(clk), .in(in), .load(load6), .address(address[8:0]), .out(out6));
-	RAM512 ram7(.clk(clk), .in(in), .load(load7), .address(address[8:0]), .out(out7));
+	initial begin
+		integer ii;
+		for(ii = 0; ii < 4096; ii = ii + 1) begin
+			RAM[ii] <= 16'd0;
+		end
+	end
 
-	// ロード、またはリードしたout0～7をアドレスをセレクタにしてoutに出力する
-	Mux8Way16 mux8(
-		.a(out0), .b(out1), .c(out2), .d(out3),
-		.e(out4), .f(out5), .g(out6), .h(out7),
-		.sel(address[11:9]),
-		.out(out));
+	always @(posedge clk) begin
+		if(load == 1'b1) begin
+			RAM[address] <= in;
+		end
+	end
+
+	assign out = RAM[address];
+
+	// // アドレスをセレクタとして、load値をload0～7に振り分ける
+	// wire load0,load1,load2,load3,load4,load5,load6,load7;
+	// DMux8Way dmux8(
+	// 	.in(load),
+	// 	.sel(address[11:9]),
+	// 	.a(load0), .b(load1), .c(load2), .d(load3),
+	// 	.e(load4), .f(load5), .g(load6), .h(load7));
+
+	// // load0～7でRegister0～7をロード、またはリードする
+	// wire[15:0] out0,out1,out2,out3,out4,out5,out6,out7;
+	// RAM512 ram0(.clk(clk), .in(in), .load(load0), .address(address[8:0]), .out(out0));
+	// RAM512 ram1(.clk(clk), .in(in), .load(load1), .address(address[8:0]), .out(out1));
+	// RAM512 ram2(.clk(clk), .in(in), .load(load2), .address(address[8:0]), .out(out2));
+	// RAM512 ram3(.clk(clk), .in(in), .load(load3), .address(address[8:0]), .out(out3));
+	// RAM512 ram4(.clk(clk), .in(in), .load(load4), .address(address[8:0]), .out(out4));
+	// RAM512 ram5(.clk(clk), .in(in), .load(load5), .address(address[8:0]), .out(out5));
+	// RAM512 ram6(.clk(clk), .in(in), .load(load6), .address(address[8:0]), .out(out6));
+	// RAM512 ram7(.clk(clk), .in(in), .load(load7), .address(address[8:0]), .out(out7));
+
+	// // ロード、またはリードしたout0～7をアドレスをセレクタにしてoutに出力する
+	// Mux8Way16 mux8(
+	// 	.a(out0), .b(out1), .c(out2), .d(out3),
+	// 	.e(out4), .f(out5), .g(out6), .h(out7),
+	// 	.sel(address[11:9]),
+	// 	.out(out));
 
 endmodule
