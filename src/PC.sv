8f539cf85fd6f2ee5466e895352a822e83187c64
dgkzoo <dgookz@gmail.com>
2018-11-28 00:53:03 +0900

    refactor

--- src/PC.sv
+++ src/PC.sv
@@ -8,12 +8,8 @@ module PC(
 	input wire reset,			// reset flg
 	output wire[15:0] out);		// out
 
-	wire[15:0] regOut, toReg;
-	Register reg1(.clk(clk), .in(toReg), .load(1'b1), .out(out));
-	assign regOut = out;
-
 	// インクリメント	
-	wire[15:0] incOut;
+	wire[15:0] regOut, incOut;
 	Inc16 inc16(.a(regOut), .out(incOut));
 
 	// inc値により、Register値かインクリメント値かをregOrIncOutに配線
@@ -26,6 +22,10 @@ module PC(
 
 	// reset値により、loadOutか0かをtoRegに配線
 	// toRegはInc16にフィードバックされる
+	wire[15:0] toReg;
 	Mux16 mux3(.a(loadOut), .b(16'b0), .sel(reset), .out(toReg));
+
+	Register reg1(.clk(clk), .in(toReg), .load(1'b1), .out(out));
+	assign regOut = out;
 
 endmodule
